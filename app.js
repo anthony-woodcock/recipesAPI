72ca5efb881c0b3bfbbe6fa623f460d29b87de89
Paul <ramshaw@hotmail.co.uk>
2017-08-28 19:33:39 +0100

    initial commit with basic configuration

---
+++ app.js
@@ -0,0 +1,19 @@
+var path = require('path')
+require('dotenv').config({
+    path: path.join(__dirname, 'settings.env')
+})
+
+var mongoose =require('mongoose')
+mongoose.connect(process.env.DATABASE_URL, {
+    useMongoClient: true
+})
+
+var express = require('express')
+var bodyParser = require('body-parser')
+var app = express()
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+app.listen(3000, function(){
+    console.log('listening on port 3000')
+})
